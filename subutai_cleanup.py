b3429765c079c9d3dc2cf809a372c9472470fc3d
Fernando Henrique <liquuid@gmail.com>
2018-03-02 14:17:35 -0300

    cleanup adding correct parameter

--- subutai_cleanup.py
+++ subutai_cleanup.py
@@ -17,6 +17,12 @@ version_added: "2.5"
 description:
     - "Cleanup subutai enviroment"
 
+options:
+    vlan:
+        description:
+            - name of vlan
+        required: true
+
 extends_documentation_fragment:
     - subutai
 
@@ -28,6 +34,7 @@ EXAMPLES = '''
 # cleanup enviroment
 - name: cleanup enviroment
   subutai_cleanup:
+    vlan: 101
 
 '''
 
@@ -45,14 +52,20 @@ from ansible.module_utils.basic import AnsibleModule
 
 def run_module():
 
+    # parameters
+    module_args = dict(
+        vlan=dict(type='str', required=True),
+    )
+
     # skell to result
     result = dict(
         changed=False,
+        vlan='',
         message=''
     )
 
     module = AnsibleModule(
-        argument_spec=dict(),
+        argument_spec=module_args,
         supports_check_mode=True,
     )
 
@@ -60,11 +73,15 @@ def run_module():
     if module.check_mode:
         return result
 
+    result['vlan'] = module.params['vlan']
+
     # verify if container is already started
     err = subprocess.Popen(
-        ["/snap/bin/subutai", "cleanup"], stderr=subprocess.PIPE).stderr.read()
+        ["/snap/bin/subutai", "cleanup",  module.params['vlan']], stderr=subprocess.PIPE).stderr.read()
     if err:
+        result['changed'] = False
         module.fail_json(msg='[Err] ' + err, **result)
+
     result['changed'] = True
 
     module.exit_json(**result)
